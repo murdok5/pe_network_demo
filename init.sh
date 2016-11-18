edc0bc8a11d2243c0507c4aa5eb4469b5cf344f2
murdok5 <mike.smith@puppet.com>
2016-11-18 15:44:09 -0800

    first commit

---
+++ init.sh
@@ -0,0 +1,13 @@
+# This scrip creates 3 vmpooler machines and outputs their names.  
+# machine1 - centos7, PE Master
+# machine2 - cisco device
+# machine3 - arista device
+
+# prereq - https://confluence.puppetlabs.com/pages/viewpage.action?pageId=28610683
+
+
+$master = floaty get centos-7-x86_64
+$cisco = floaty get cisco-nxos-9k-x86_64
+$arista = floaty get arista-4-i386
+
+
