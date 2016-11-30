1fcfc7b579a94cdb576c2f27d8c058599f13353a
Rick Sherman <rick@shermdog.com>
2016-11-30 12:53:06 -0600

    Add Arista API configuration

--- arista_install.sh
+++ arista_install.sh
@@ -1,5 +1,12 @@
-# login as 'admin'
+# login as 'admin' and enable API + install puppet-agent
 enable
+configure terminal
+management api http-commands
+no protocol https
+protocol unix-socket
+no shutdown
+end
+copy running-config startup-config
 copy https://s3.amazonaws.com/puppet-agents/2016.4/puppet-agent-latest/repos/eos/4/PC1/i386/puppet-agent-1.7.1-1.eos4.i386.swix extension:
 extension puppet-agent-1.7.1-1.eos4.i386.swix
 # log in as root
